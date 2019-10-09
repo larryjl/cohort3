cb85e92f7f05e1edc7585d85cd7f197d57744d35
larryjl <leelawrencej@gmail.com>
2019-10-08 21:52:34 -0600

    finished define functions testing all variable types. test if else stud.

--- src/javascript/syntax.js
+++ src/javascript/syntax.js
@@ -1,32 +1,40 @@
 const functions = {
 
 // define attributes / variables
-    define: () => {
-        return 0;
-    }
-// number
-// string
-// boolean
-// array
-// dictionary / objects
-// undefined
+//  number
+//  string
+//  boolean
+//  array
+//  dictionary / objects
+//  undefined
+define: (a) => {
+    return a;
+},
+
 // sample if / else
+ifElse: () => {
+    return undefined;
+}
+
 // functions
-// parameters
-// returns
+//  parameters
+//  returns
+
 // arrays
-// add to the front
-// add to the end
-// update values
+//  add to the front
+//  add to the end
+//  update values
+
 // loops 
-// for
-// for/in
-// while
-// do while
-// forEach (with array and function)
+//  for
+//  for/in
+//  while
+//  do while
+//  forEach (with array and function)
+
 // Objects / Dictionaries
-// declare object
-// lookup key to retrieve value
+//  declare object
+//  lookup key to retrieve value
 };
 
 export default functions;
--- src/javascript/syntax.test.js
+++ src/javascript/syntax.test.js
@@ -1,5 +1,14 @@
 import functions from './syntax';
 
-test('test comment', () => {
-    expect(functions.define()).toBe(1);
+test('test return variable types', () => {
+    expect(functions.define(1)).toBe(1);
+    expect(functions.define('string')).toBe('string');
+    expect(functions.define(true)).toBe(true);
+    expect(JSON.stringify(functions.define(['a',2]))).toBe(JSON.stringify(['a',2]));
+    expect(JSON.stringify(functions.define({a:1,b:2}))).toBe(JSON.stringify({a:1,b:2}));
+    expect(functions.define(undefined)).toBe(undefined);
+});
+
+test('test if else', () => {
+    expect(functions.ifElse()).toBe(1);
 });
