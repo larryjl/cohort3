6b2764aedc01f5891d6814f92703841ae99587e7
larryjl <leelawrencej@gmail.com>
2019-10-10 17:27:37 -0600

    calculator placeholder added to getting started. added daily 2019-10-09 (email from array)

--- src/javascript/daily/daily.js
+++ src/javascript/daily/daily.js
@@ -1,37 +1,54 @@
-/*    
-    Write the function that will create this output:
+const functions = {
 
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are not the same:
-    p1--> 2
-    p2--> 2
-*/
+    // *** 2019-10-07 ***
 
-// Write the function after this comment ---
+    /*    
+        Write the function that will create this output:
+
+    *** the two values are not the same:
+        p1--> a
+        p2--> b
+    *** the two values are not the same:
+        p1--> 1
+        p2--> 2
+    *** the two values are not the same:
+        p1--> 2
+        p2--> 2
+    */
+
+    // Write the function after this comment ---
 
 
-const assertEquals = (a,b) => {
-    if (!(a===b)) {
-        console.log(
-            ` *** the two values are not the same
-            p1-->${a}
-            p2-->${b}`);
+    assertEquals: (a,b) => {
+        if (!(a===b)) {
+            console.log(
+                ` *** the two values are not the same
+                p1-->${a}
+                p2-->${b}`);
+        }
+        return (a===b) ? true : false;
+    },
+
+    // and before this comment ---
+
+    // assertEquals("a","b");
+    // assertEquals("a","a");
+    // assertEquals(1,2);
+    // assertEquals(2,2);
+    // assertEquals("2",2);
+    // assertEquals("This value","This value");
+
+
+    // *** 2019-10-09 ***
+
+    makeEmailArr: (arr) => {
+        const firstName = arr[0].toLowerCase();
+        const lastName = arr[1].toLowerCase();
+        const email = `${firstName}.${lastName}@evolveu.ca`;
+        return email;
     }
-    return (a===b) ? true : false;
-}
-export default assertEquals;
 
 
-// and before this comment ---
+};
 
-assertEquals("a","b");
-assertEquals("a","a");
-assertEquals(1,2);
-assertEquals(2,2);
-assertEquals("2",2);
-assertEquals("This value","This value");
+export default functions;
--- src/javascript/daily/daily.test.js
+++ src/javascript/daily/daily.test.js
@@ -1,11 +1,21 @@
-import assertEquals from './daily'
+import functions from './daily.js'
 
 test('Check equality', () => {
-    expect(assertEquals('a','b')).toBe(false);
-    expect(assertEquals('a','a')).toBe(true);
-    expect(assertEquals(1,2)).toBe(false);
-    expect(assertEquals(2,2)).toBe(true);
-    expect(assertEquals('2',2)).toBe(false); // tbd
-    expect(assertEquals(2.0,2)).toBe(true);
-    expect(assertEquals('this value','this value')).toBe(true);
+    expect(functions.assertEquals('a','b')).toBe(false);
+    expect(functions.assertEquals('a','a')).toBe(true);
+    expect(functions.assertEquals(1,2)).toBe(false);
+    expect(functions.assertEquals(2,2)).toBe(true);
+    expect(functions.assertEquals('2',2)).toBe(false); // tbd
+    expect(functions.assertEquals(2.0,2)).toBe(true);
+    expect(functions.assertEquals('this value','this value')).toBe(true);
+});
+
+test('email builder from an array', () => {
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
 });
