bfc3ab273c0deb7639e6af5bf92c3b2a84f3fbcb
larryjl <leelawrencej@gmail.com>
2019-11-20 20:04:24 -0700

    completed react-01 components

---
+++ react-01/src/components/OddComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyComp extends React.Component {
+
+  render() {
+    return (
+      <div>
+      <h1>This is the OddComp</h1>
+      </div>
+    )
+  }
+}
+
+export default MyComp;
