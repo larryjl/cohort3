bd08def30f6b1c165ce43d5d96c804e1b3cc41b2
larryjl <leelawrencej@gmail.com>
2019-12-12 18:57:42 -0700

    refactored Nav to SPAs instead of vertical scroll activeLink in Nav state moved to activePage in App state.

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -3,53 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/NavComp';
 import Game from './components/TicComp';
+import Transform from './components/TransformComp';
 import Accounts from './components/AccountsComp';
 import Cities from './components/CitiesComp';
 
 class App extends Component {
+
   constructor() {
     super();
     this.state = {
+      activePage: "home"
     };
-  }
+  };
 
-  render () {
+  setPage = (page) => {
+    this.setState({
+      activePage: page
+    });
+  };
+
+  render() {
+    let main;
+    switch(this.state.activePage){
+      case "game":
+        main = <Game/>
+        break;
+      case "transform":
+        main = <Transform/>
+        break;
+      case "accounts":
+        main = <Accounts/>
+        break;
+      case "cities":
+        main = <Cities/>
+        break;
+      default: // home
+        main = 
+          <div className="main--home">
+            <img src={logo} className="logo bounceIn" alt="logo" />
+          </div>;
+    };
     return (
-      <div className="App">
-        <a name="top">
+      <div className="app">
         <Nav
+          setPage={this.setPage}
         />
-        </a>
-        <header className="App-header svgStyle">
-          <img src={logo} className="App-logo bounceIn" alt="logo" />
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-        </header>
         <main>
-          <hr/>
-          <a name="game">
-            <Game
-            />
-          </a>
-          <hr/>
-          <a name="accounts">
-            <Accounts
-            />
-          </a>
-          <hr/>
-          <a name="cities">
-            <Cities
-            />
-          </a>
+          {main}
         </main>
         <footer>
         </footer>
--- react-02/src/components/Nav.js
+++ react-02/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './NavComp.css';
 import logoSvg from '../logo.svg';
+import { ReactComponent as IconExit } from '../svg/Icon_exit.svg';
 import { ReactComponent as IconCard } from '../svg/Icon_card.svg';
 import { ReactComponent as IconUserCircle } from '../svg/Icon_user_circle.svg';
 
@@ -8,37 +9,23 @@ import { ReactComponent as IconUserCircle } from '../svg/Icon_user_circle.svg';
 class Nav extends React.Component {  
   constructor(props) {
     super(props);
-    this.state = {
-      activeLink: 0
-    };
+    this.state = {};
   };
-  setActive = (link) => {
-    this.setState({activeLink: link});
+
+  handleClick = (page) => {
+    this.props.setPage(page)
   };
-  linkClick = () => {
-    this.setActive()
-  }
+
   render() {
     return (
       <nav id="idNav">
-        {/* img */}
-        <a href="#top" onClick={() => {
-          this.setActive(0);
-          }}>
-          <img src={logoSvg} tabIndex="0" alt="home" className={
-            "icon home"+
-            ((this.state.activeLink===0)? " icon--active" : "")
-          } />
-        </a>
+        {/* img src */}
+        <button key="home" onClick={() => this.handleClick("home")}>
+          <img src={logoSvg} tabIndex="0" alt="home" className="icon home" />
+        </button>
         {/* inline with focus */}
-        <a href="#game" onClick={() => {this.setActive(1)}}>
-          <svg 
-            className={
-            "rotating icon"+
-            ((this.state.activeLink===1)? " icon--active" : "")
-            } 
-            tabIndex="0" alt ="tic-tac-toe" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-          >
+        <button key="game" onClick={() => this.handleClick("game")}>
+          <svg className={"rotating icon"} tabIndex="0" alt ="tic-tac-toe" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M4 5C4 4.44772 4.44772 4 5 4H7C7.55228 4 8 4.44772 8 5V7C8 7.55228 7.55228 8 7 8H5C4.44772 8 4 7.55228 4 7V5Z" fill="black"/>
             <path d="M10 5C10 4.44772 10.4477 4 11 4H13C13.5523 4 14 4.44772 14 5V7C14 7.55228 13.5523 8 13 8H11C10.4477 8 10 7.55228 10 7V5Z" fill="black"/>
             <path d="M17 4C16.4477 4 16 4.44772 16 5V7C16 7.55228 16.4477 8 17 8H19C19.5523 8 20 7.55228 20 7V5C20 4.44772 19.5523 4 19 4H17Z" fill="black"/>
@@ -49,20 +36,17 @@ class Nav extends React.Component {
             <path d="M10 17C10 16.4477 10.4477 16 11 16H13C13.5523 16 14 16.4477 14 17V19C14 19.5523 13.5523 20 13 20H11C10.4477 20 10 19.5523 10 19V17Z" fill="black"/>
             <path d="M17 16C16.4477 16 16 16.4477 16 17V19C16 19.5523 16.4477 20 17 20H19C19.5523 20 20 19.5523 20 19V17C20 16.4477 19.5523 16 19 16H17Z" fill="black"/>  
           </svg>
-        </a>
+        </button>
         {/* react 2.0 svg */}
-        <a href="#accounts" onClick={() => {this.setActive(2)}}>
-          <IconCard tabIndex="0" alt="accounts" className={
-            "rotating icon"+
-            ((this.state.activeLink===2)? " icon--active" : "")
-          }/>
-        </a>
-        <a href="#cities" onClick={() => {this.setActive(3)}}>
-          <IconUserCircle tabIndex="0" alt="cities" className={
-            "rotating icon"+
-            ((this.state.activeLink===3)? " icon--active" : "")
-          }/>
-        </a>
+        <button key="transform" onClick={() => this.handleClick("transform")}>
+          <IconExit tabIndex="0" alt="transform" className="rotating icon"/>
+        </button>
+        <button key="accounts" onClick={() => this.handleClick("accounts")}>
+          <IconCard tabIndex="0" alt="accounts" className="rotating icon"/>
+        </button>
+        <button key="cities" onClick={() => this.handleClick("cities")}>
+          <IconUserCircle tabIndex="0" alt="cities" className="rotating icon"/>
+        </button>
       </nav>
     );
   };
