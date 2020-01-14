b6b67dd1c4b8f1f15100e034635d6431cd1a313e
larryjl <leelawrencej@gmail.com>
2020-01-13 22:52:09 -0700

    feat: direction functions: forward, left, right

--- react-02/src/components/fifoFunctions.js
+++ react-02/src/components/fifoFunctions.js
@@ -1,12 +1,30 @@
 const functions = {
-  forward() {
-    
+  forward(point, distance, direction) {
+    // direction is an array: [1,0], [-1,0], [0,1] or [0,-1]
+    return (point.map((v,i) => (
+      v + (distance * direction[i])
+    )));
   },
-  turnLeft() {
-
+  turnLeft(prevDirection) {
+    let [x, y] = prevDirection;
+    // convert signed -0 to 0
+    return [(-y)?-y:0, x];
+    // switch (prevDirection) {
+    //   case [0,1]:
+    //     return [-1,0];
+    //   case [-1,0]:
+    //     return [0,-1];
+    //   case [0,-1]:
+    //     return [1,0];
+    //   case [1,0]:
+    //     return [0,1];
+    //   default:
+    //     return [-1,0];
+    // };
   },
-  turnRight() {
-
+  turnRight(prevDirection) {
+    let [x, y] = prevDirection;
+    return [y, (-x)?-x:0];
   }
 };
 
