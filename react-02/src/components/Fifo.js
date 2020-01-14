3267743670214fb5f1caeb4856470497589be63a
larryjl <leelawrencej@gmail.com>
2020-01-13 18:39:41 -0700

    feat: add directional buttons using input comps

--- react-02/src/components/Fifo.js
+++ react-02/src/components/Fifo.js
@@ -1,23 +1,66 @@
 import React, {useState, useEffect} from 'react';
-import {Stack} from './stack';
+// import Stack from './stack';
 import {RecursiveStack} from './queue';
+import fifoFunctions from './fifoFunctions';
+import inputs from './inputs';
+import { ReactComponent as IconUp } from '../svg/Icon_up.svg';
+import { ReactComponent as IconLeft } from '../svg/Icon_left.svg';
+import { ReactComponent as IconRight } from '../svg/Icon_right.svg';
 import styles from './Fifo.module.css';
 
+const {Button,} = inputs;
+
+const stack = new RecursiveStack();
+
 function Fifo(props) {
-  const [state, setState] = useState();
+  const [inputs, setInputs] = useState({});
   useEffect(() => {
     // side effect
     return function cleanup() {
       // cleanup
     };
   });
+  const callbacks = {
+    forward: {f: fifoFunctions.forward, p: []},
+    turnLeft: {f: fifoFunctions.turnLeft, p: []},
+    turnRight: {f: fifoFunctions.turnRight, p: []}
+  };
+  const icons = {
+    forward: IconUp,
+    turnLeft: IconLeft,
+    turnRight: IconRight
+  };
+  const buttons = (
+    <div>
+      {Object.keys(callbacks).map((v,i) => {
+        const Icon = icons[v];
+        return (
+          <Button
+            key={i}
+            id={i}
+            name={v}
+            label={<Icon
+              name={v}
+              alt={v + ' button'}
+              tabIndex="0"
+              className={[styles.icon, styles.arrow].join(' ')}
+            />}
+            callbacks={callbacks}
+            setInputs={setInputs}
+            classes={styles.button}
+          />
+        )
+      })}
+    </div>
+  );
   return (<main id="idMainLink">
     <h2>Stacks and Queues</h2>
+    {buttons}
     <div id={styles.container}>
-      <StackComp
+      {/* <StackComp
       />
       <QueueComp
-      />
+      /> */}
     </div>
   </main>);
 };
