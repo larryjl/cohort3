7589f83e28398a804edbcd1c42bcedb20ebe8cf8
larryjl <leelawrencej@gmail.com>
2020-01-20 01:16:26 -0700

    feat: variable and if/else functions and tests

---
+++ python/syntax.py
@@ -0,0 +1,100 @@
+# # // define attributes / variables
+# # //  number
+# # //  string
+# # //  boolean
+# # //  array
+# # //  dictionary / objects
+# # //  undefined
+def define(a):
+    b=a
+    return b
+
+# # // sample if / else
+def ifElse(bool):
+    if bool: 
+        return True 
+    else:
+        return  False
+
+# # // functions
+# # //  parameters
+# # //  returns
+# funParameters: (a) => {
+#     let foo = (b) => b;
+#     return foo(a);
+# }
+
+# # // arrays
+# funArray: (array,first,last) => {
+#     //  add to the front
+#     array.unshift(first);
+#     //  add to the end
+#     array.push(last);
+#     //  update values
+#     return array;
+# }
+
+# # // loops 
+# # // for
+# funFor: (a) => {
+#     for (let i=0;i<3;i++) { // loop 3x
+#         a++;
+#     }; // add 3 to a
+#     return a;
+# }
+
+# # // for/in
+# funForIn: (a) => {
+#     let obj = {a:'a',b:'b'};
+#     for (let i in obj) {
+#         obj[i]=obj[i]+a;
+#     }; 
+#     return obj;
+# }
+
+# # // while 
+# funWhile: (a) => {
+#     let n=0;
+#     while (n<3) { // loop 3x
+#         a++;
+#         n++;
+#     }; // add 3 to a
+#     return a;
+# }
+
+# # // do while
+# funDoWhile: (a) => {
+#     let n=0;
+#     do {
+#         a++;
+#         n++;
+#     } while (n<3); // add 3 to a
+#     return a;
+# }
+
+# # // forEach
+# funForEach: (x) => {
+#     let arr = ['a','b'];
+#     let str = x.toString();
+#     arr.forEach( function(v) {
+#         str=str+v;
+#     }); // e.g. '0ab'
+#     return str;
+# }
+
+# # // Objects / Dictionaries
+# funObject: (arr) => {
+#     //  declare object
+#     let obj=[];
+#     arr.forEach( function(v, i) {
+#         obj[i]=v;
+#     }); // e.g. obj = {0:'a',1:'b'}
+#     //  lookup key to retrieve value
+#     let values = Object.values(obj);
+#     let keys = Object.keys(obj);
+#     let str = '';
+#     for (let i in obj) {
+#         str = str + obj[i];
+#     };
+#     return str;
+# }
---
+++ python/syntax_test.py
@@ -0,0 +1,59 @@
+import pytest
+import syntax
+
+class TestVariable:
+    def test1(self):
+        assert syntax.define(1)==1
+
+    def test2(self):
+        assert syntax.define('string')=='string'
+
+    def test3(self):
+        assert syntax.define(True)==True
+
+    def test4(self):
+        assert syntax.define(['a',1])==['a',1]
+
+    def test5(self):
+        assert syntax.define({'a':1,'b':2})=={'a':1,'b':2}
+
+    def test6(self):
+        assert syntax.define(None)==None
+
+class TestIfElse:
+    def test7(self):
+        assert syntax.ifElse(True)==True
+    def test8(self):
+        assert syntax.ifElse(False)==False
+        
+# test('test function with parameters', () => {
+#     expect(functions.funParameters('a')).toBe('a');
+# });
+
+# test('test array manipulation', () => {
+#     expect(JSON.stringify(functions.funArray([0,1],'a','z'))).toBe(JSON.stringify(['a',0,1,'z']));
+# });
+
+# test('test function with for loop', () => {
+#     expect(functions.funFor(0)).toBe(3);
+# });
+
+# test('test function with for in loop', () => {
+#     expect(JSON.stringify(functions.funForIn(0))).toBe(JSON.stringify({a:'a0',b:'b0'}));
+# });
+
+# test('test function with while loop', () => {
+#     expect(functions.funWhile(0)).toBe(3);
+# });
+
+# test('test function with do-while loop', () => {
+#     expect(functions.funDoWhile(0)).toBe(3);
+# });
+
+# test('test function with for-each loop', () => {
+#     expect(functions.funForEach(0)).toBe('0ab');
+# });
+
+# test('test objects', () => {
+#     expect(functions.funObject(['a','b'])).toBe('ab');
+# });
