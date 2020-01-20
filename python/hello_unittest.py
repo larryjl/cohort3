6f96151385dee522700d6d4dfcbd17f7c727d990
larryjl <leelawrencej@gmail.com>
2020-01-20 00:43:47 -0700

    fix: test class name change

--- python/hello_unittest.py
+++ python/hello_unittest.py
@@ -1,7 +1,7 @@
 import hello    # The code to test
 import unittest   # The test framework
 
-class Test_TestIncrementDecrement(unittest.TestCase):
+class Test_Hello(unittest.TestCase):
     def test_increment(self):
         self.assertEqual(hello.hello(), "Hello World")
 
