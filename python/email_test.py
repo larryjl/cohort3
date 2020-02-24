d561875f0258e19c0739bdc25bcd04bf3a9d22b6
larryjl <leelawrencej@gmail.com>
2020-02-23 18:46:13 -0700

    feat: email tdd green

--- python/email_test.py
+++ python/email_test.py
@@ -1,7 +1,10 @@
 import pytest
 
+
 def email(firstName, lastName):
-    return True
+    return f"{firstName}.{lastName}@evolveu.ca"
+
 
 def test_email_sameCase():
-    assert email('Larry', 'Shumlich') == "Larry.Shumlich@evolveu.ca"
+    assert email("Larry", "Shumlich") == "Larry.Shumlich@evolveu.ca"
+
