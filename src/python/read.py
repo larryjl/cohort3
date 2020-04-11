939cfa91e7262330a8e258ab0aba486f6f56e6a2
larryjl <leelawrencej@gmail.com>
2020-04-10 23:50:06 -0600

    feat: get csv fields

--- src/python/read.py
+++ src/python/read.py
@@ -32,8 +32,33 @@ def folderFiles(path):
         if os.path.isdir(fpath):
             continue
         fsize = os.stat(fpath).st_size
-        fileList.append(
-            {"filename": fname, "size": fsize}
-        )   
+        fileList.append({"filename": fname, "size": fsize})
         print(f"Filename: {fname:20} Size: {fsize:6} Bytes")
     return fileList
+
+
+# write a python program that will:
+# only read the csv file once. Do not load the file into memory and then process it. The intent of this exercise is to pretend that the file is so massive it can only be read once and can not fit into memory.
+# use a dictionary to total “res_cnt” by “CLASS” and “SECTOR”. Do not use lists, or sort the file, or any other library. You do not know from execution to execution what the Class or Sector names will be. Write the code so there is only one loop through the data.
+# Create a total line for each of the following independently:
+# CLASS
+# SECTOR
+# count the number of lines
+# print a nice little report at the end
+# as a stretch goal; can you do this with no “if” statement
+# write the report to a file called report.txt.
+
+
+def lineToList(line):
+    return [item.strip() for item in line.split(",")]
+
+
+def communityReport(path, selectFields):
+    with open("data/Census_by_Community_2019.csv", "r") as f:
+        i = 0
+        for line in f:
+            if i == 0:
+                fields = lineToList(line)
+                indices = [fields.index(item) for item in selectFields]
+            i += 1
+    return i, len(fields), len(indices)
--- src/python/read_test.py
+++ src/python/read_test.py
@@ -1,21 +1,36 @@
 import pytest
 import read
 
+
 class Test_read:
-    path = '../javascript/syntax.js'
+    path = "../javascript/syntax.js"
+
     def test_read(self):
-        assert read.read(self.path) == 'const func'
+        assert read.read(self.path) == "const func"
+
     def test_lines(self):
-        path = '../javascript/syntax.js'
         assert read.lines(self.path) == 105
+
     def test_substrings(self):
-        path = '../javascript/syntax.js'
         assert read.substrings(self.path, "for") == 5
 
 
 class Test_folder:
-    path = '../javascript/'
+    path = "../javascript/"
+
     def test_lines(self):
         result = read.folderFiles(self.path)
         print(result)
         assert len(result) == 12
+
+
+class Test_csv:
+    path = "./data/Census_by_Community_2019.csv"
+    fields = ("CLASS", "SECTOR")
+
+    def test_lineToList(self):
+        assert read.lineToList("a,  b,  c,  d  ,  ") == ["a", "b", "c", "d", ""]
+
+    def test_csv(self):
+        result = read.communityReport(self.path, self.fields)
+        assert result == (307, 142, 2)
