74ca9b8361600ee6891bd9a7a36e9d860e27e16f
larryjl <leelawrencej@gmail.com>
2020-04-17 23:33:33 -0600

    feat: validation and merge

--- src/python/readExcel.py
+++ src/python/readExcel.py
@@ -2,21 +2,38 @@ import os
 from openpyxl import load_workbook
 
 
-def workbookToDict(path):
+def workbookToDict(path, sourceNum=None, boundsDict=None):
     wb = load_workbook(filename=path, read_only=True, data_only=True)
     wbDict = {}
     for ws in wb:
         wbDict[ws.title] = {}
-        for rowNum, rowValues in enumerate(ws.values):
+        sheetBounds = {"minRow": None, "maxRow": None, "minCol": None, "maxCol": None}
+        if boundsDict and (ws.title in boundsDict):
+            for key, value in boundsDict[ws.title].items():
+                sheetBounds[key] = value
+        for rowNum, rowValues in enumerate(
+            ws.iter_rows(
+                sheetBounds["minRow"],
+                sheetBounds["maxRow"],
+                sheetBounds["minCol"],
+                sheetBounds["maxCol"],
+                True,
+            )
+        ):
             if rowNum == 0:
+                duplicate = duplicateCheck(rowValues)
+                if duplicate:
+                    raise Exception(f"Workbook has duplicates: {duplicate}")
                 headings = rowValues
-                ## todo: check for duplicates
-                ## todo: deal with None
-                ## todo: specify table location
+                ## todo: ignore extra None columns
             else:
-                wbDict[ws.title][rowNum] = {}
+                if sourceNum != None:
+                    key = f"{sourceNum}-{rowNum}"
+                else:
+                    key = rowNum
+                wbDict[ws.title][key] = {}
                 for colNum, cellValue in enumerate(rowValues):
-                    wbDict[ws.title][rowNum][headings[colNum]] = cellValue
+                    wbDict[ws.title][key][headings[colNum]] = cellValue
     return wbDict
 
 
@@ -29,51 +46,92 @@ def dictToString(lineDict, keys, keyLength, valueLength, separator=", "):
     return string[:-2]
 
 
-def matching(wbDict, sheet, matchKey, matchValue):
-    idList = [
-        rowItem[0]
-        for rowItem in wbDict[sheet].items()
-        if rowItem[1][matchKey] == matchValue
-    ]
+def matching(wbDict, sheet, matchKey, matchValue, idKey=None):
+    if idKey != None:
+        idList = [
+            row[idKey]  # idKey value
+            for row in wbDict[sheet].values()
+            if row[matchKey] == matchValue
+        ]
+    else:
+        idList = [
+            rowKey  # row number
+            for rowKey, rowValue in wbDict[sheet].items()
+            if rowValue[matchKey] == matchValue
+        ]
     return idList
+
+
+def duplicateCheck(alist):
+    listSet = set()
+    for item in alist:
+        if item in listSet:
+            return item
+        elif item != None:
+            listSet.add(item)
+        else:
+            continue
+    return False
+
+
+def mergeWorkbooksToDict(pathDict):
+    for sourceNum, (path, boundsDict) in enumerate(pathDict.items()):
+        newDict = workbookToDict(path, sourceNum, boundsDict)
+        if sourceNum == 0:
+            wbDict = newDict
+        else:
+            for sheetName, sheetDict in newDict.items():
+                if sheetName in wbDict:
+                    wbDict[sheetName].update(sheetDict)
+                    ## Todo: remove duplicates
+                else:
+                    wbDict[sheetName] = sheetDict
+    return wbDict
 
 
 def jobReport(
     wbDict,
-    jobSheet,
     jobId,
-    jobFields,
-    companyField,
-    connectionSheet,
-    connectionFields,
-    jobIdField,
-    jobKeywordSheet,
-    keywordField,
-    keywordSheet,
-    keywordFields,
-    title,
-    outPath
+    jobSheet="Jobs",
+    IdField="ID",
+    jobFields=("Company", "Location", "Job Title", "Application Date"),
+    companyField="Company",
+    connectionSheet="Connections",
+    connectionFields=("Name", "Job Title"),
+    jobIdField="JobID",
+    jobKeywordSheet="Job-Keywords",
+    keywordField="Keyword",
+    keywordSheet="Keywords",
+    keywordFields=("Keyword", "Type"),
+    title="Job",
+    keyLength = 10,
+    valueLength = 20
 ):
-    ## todo: differentiate between row num and id value, or make them the same in the dictionary creation
-    jobDict = wbDict[jobSheet][jobId]
-    jobInfo = dictToString(jobDict, jobFields, 10, 10,"  \r\n")
+    jobNum = matching(wbDict, jobSheet, IdField, jobId)[0]
+    jobDict = wbDict[jobSheet][jobNum]
+    jobInfo = dictToString(jobDict, jobFields, keyLength, valueLength, "  \r\n")
     company = jobDict[companyField]
 
-    connectionIdList = matching(wbDict, connectionSheet, companyField, company)
+    connectionNumList = matching(wbDict, connectionSheet, companyField, company)
+
     connectionStrList = [
-        "- " + dictToString(wbDict[connectionSheet][id], connectionFields, 10, 10)
-        for id in connectionIdList
+        "- " + dictToString(wbDict[connectionSheet][id], connectionFields, keyLength, valueLength)
+        for id in connectionNumList
     ]
     connectionInfo = "  \r\n".join(connectionStrList)
 
-    jobKeywordIdList = matching(wbDict, jobKeywordSheet, jobIdField, jobId)
-    
-    keywordList = [wbDict[jobKeywordSheet][id][keywordField] for id in jobKeywordIdList]
+    jobKeywordNumList = matching(wbDict, jobKeywordSheet, jobIdField, jobId)
+
+    keywordList = [
+        wbDict[jobKeywordSheet][id][keywordField] for id in jobKeywordNumList
+    ]
 
     keywordStrList = []
     for keyword in keywordList:
         keywordId = matching(wbDict, keywordSheet, keywordField, keyword)[0]
-        keywordString = "- " + dictToString(wbDict[keywordSheet][keywordId], keywordFields, 10, 10)
+        keywordString = "- " + dictToString(
+            wbDict[keywordSheet][keywordId], keywordFields, keyLength, valueLength
+        )
         keywordStrList.append(keywordString)
     keywordInfo = "  \r\n".join(keywordStrList)
 
@@ -87,6 +145,43 @@ def jobReport(
             keywordInfo,
         )
     )
-    with open(outPath, "w") as f:
-        f.write(string)
     return string
+
+
+def reportJobWorkbooks(
+    pathDict,
+    outPath,
+    jobId,
+    jobSheet="Jobs",
+    IdField="ID",
+    jobFields=("Company", "Location", "Job Title", "Application Date"),
+    companyField="Company",
+    connectionSheet="Connections",
+    connectionFields=("Name", "Job Title"),
+    jobIdField="JobID",
+    jobKeywordSheet="Job-Keywords",
+    keywordField="Keyword",
+    keywordSheet="Keywords",
+    keywordFields=("Keyword", "Type"),
+    title="Job",
+):
+    wbDict = mergeWorkbooksToDict(pathDict)
+    report = jobReport(
+        wbDict,
+        jobId,
+        jobSheet,
+        IdField,
+        jobFields,
+        companyField,
+        connectionSheet,
+        connectionFields,
+        jobIdField,
+        jobKeywordSheet,
+        keywordField,
+        keywordSheet,
+        keywordFields,
+        title,
+    )
+    with open(outPath, "w") as f:
+        f.write(report)
+    return report
--- src/python/readExcel_test.py
+++ src/python/readExcel_test.py
@@ -2,10 +2,53 @@ import pytest
 import readExcel
 
 path = "./excel/Job Search Tracker.xlsx"
+path1 = "./excel/Job Search Tracker (1).xlsx"
+boundsDict1 = {
+    "Jobs": {"minRow": 3, "maxRow": 100, "minCol": 2, "maxCol": 100},
+    "Keywords": {"minRow": 3, "maxRow": 100, "minCol": 2, "maxCol": 100},
+    "Job-Keywords": {"minRow": 3, "maxRow": 100, "minCol": 2, "maxCol": 100},
+    "Connections": {"minRow": 3, "maxRow": 100, "minCol": 2, "maxCol": 100},
+}
 
 
 def test_dict():
     wbDict = readExcel.workbookToDict(path)
+    assert type(wbDict) == dict
+    for sheet in wbDict.values():
+        assert type(sheet) == dict
+        for row in sheet.values():
+            assert type(row) == dict
+            assert len(row) > 0
+
+
+def test_dictSourceNum():
+    wbDict = readExcel.workbookToDict(path, 1)
+    assert type(wbDict) == dict
+    for sheet in wbDict.values():
+        assert type(sheet) == dict
+        for row in sheet.values():
+            assert type(row) == dict
+            assert len(row) > 0
+
+
+def test_dictSpecifyBounds():
+    wbDict = readExcel.workbookToDict(path1, boundsDict1)
+    assert type(wbDict) == dict
+    for sheet in wbDict.values():
+        assert type(sheet) == dict
+        for row in sheet.values():
+            assert type(row) == dict
+            assert len(row) > 0
+
+
+
+def test_dictSpecifySomeBounds():
+    boundsDict = {
+        "Jobs": {"minRow": 1, "maxRow": 100},
+        "Keywords": {"minRow": 1, "minCol": 1},
+        "Job-Keywords": {"maxRow": 100, "maxCol": 100},
+    }
+    wbDict = readExcel.workbookToDict(path, boundsDict)
     assert type(wbDict) == dict
     for sheet in wbDict.values():
         assert type(sheet) == dict
@@ -20,16 +63,102 @@ def test_dictString():
     result = readExcel.dictToString(lineDict, groupFields, 10, 10)
     assert result == "Company:   Amazon    , Job Title: CSA       "
 
+
 def test_match():
     wbDict = readExcel.workbookToDict(path)
     result = readExcel.matching(wbDict, "Connections", "Company", "Amazon")
     print(result)
     assert type(result) == list
 
+
+def test_matchWithKey():
+    wbDict = readExcel.workbookToDict(path)
+    result = readExcel.matching(wbDict, "Connections", "Company", "Amazon", "ID")
+    print(result)
+    assert type(result) == list
+
+
+def test_duplicate():
+    dupList = ("a", "b", "a")
+    result = readExcel.duplicateCheck(dupList)
+    print(result)
+    assert result == "a"
+
+
+def test_duplicateNone():
+    dupList = ("a", "b", "c")
+    result = readExcel.duplicateCheck(dupList)
+    print(result)
+    assert result == False
+
+
+def test_merge():
+    pathDict = {path: None, path1: boundsDict1}
+    result = readExcel.mergeWorkbooksToDict(pathDict)
+    print(result)
+    assert type(result) == dict
+
+
 def test_report():
     wbDict = readExcel.workbookToDict(path)
+    jobFields = ("Company", "Location", "Job Title", "Application Date")
+    connectionFields = ("Name", "Job Title")
+    keywordFields = ("Keyword", "Type")
     result = readExcel.jobReport(
-        wbDict, "Jobs", 1, ("Company", "Location", "Job Title", "Application Date"), "Company", "Connections", ("Name", "Job Title"), "JobID", "Job-Keywords", "Keyword", "Keywords", ("Keyword", "Type"), "Job", "./reports/reportExcel.md"
+        wbDict,
+        1,
+        "Jobs",
+        "ID",
+        jobFields,
+        "Company",
+        "Connections",
+        connectionFields,
+        "JobID",
+        "Job-Keywords",
+        "Keyword",
+        "Keywords",
+        keywordFields,
+        "Job",
     )
+    print(result)
+    assert type(result) == str
+
+
+def test_reportDefaults():
+    wbDict = readExcel.workbookToDict(path)
+    result = readExcel.jobReport(wbDict, 1,)
+    print(result)
+    assert type(result) == str
+
+
+def test_mergeReport():
+    pathDict = {path: None, path1: boundsDict1}
+    jobFields = ("Company", "Location", "Job Title", "Application Date")
+    connectionFields = ("Name", "Job Title")
+    keywordFields = ("Keyword", "Type")
+    result = readExcel.reportJobWorkbooks(
+        pathDict,
+        "./reports/reportExcel.md",
+        1,
+        "Jobs",
+        "ID",
+        jobFields,
+        "Company",
+        "Connections",
+        connectionFields,
+        "JobID",
+        "Job-Keywords",
+        "Keyword",
+        "Keywords",
+        keywordFields,
+        "Job",
+    )
+    print(result)
+    assert type(result) == str
+
+
+def test_mergeReportDefaults():
+    pathDict = {path: None, path1: boundsDict1}
+    result = readExcel.reportJobWorkbooks(pathDict, "./reports/reportExcel.md", 1)
     print(result)
     assert type(result) == str
